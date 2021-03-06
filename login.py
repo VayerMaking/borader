ec2a855dc8b37f7d7d68d9313cf20ba0b1e1e9cc
VayerMaking <martin.vayer@gmail.com>
2021-03-06 18:48:29 +0200

    login with chrome driver

--- login.py
+++ login.py
@@ -1,26 +1,27 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
+import config
 
 options = Options()
 options.headless = False
 options.add_argument("--window-size=1920,1200")
-driver = webdriver.Firefox(options=options, executable_path=r'/home/petar/Desktop/geckodriver')
+driver = webdriver.Chrome(options=options, executable_path=config.driver_path)
 
 URL = "https://www.etoro.com/login"
 def site_login():
-	username_id = "username"
-	username = "petar.dmnv03@gmail.com"
-	password_id = "password"
-	password = "reqreqreq03"
-	sign_btn_id = "login-sts-btn-sign-in"
-
 	driver.get(URL)
 	#TODO
 	'''
 	Patherns in function find_element_by_xpath are not found.
 	'''
-	driver.find_element_by_xpath("/html/body/ui-layout/div/div/div[1]/et-login/et-login-sts/div/div/div/form/div[1]").send_keys(username)
-	driver.find_element_by_xpath("/html/body/ui-layout/div/div/div[1]/et-login/et-login-sts/div/div/div/form/div[2]").send_keys(password)
+	username = driver.find_element_by_name("username")
+	username.clear()
+	username.send_keys(config.username)
+
+	password = driver.find_element_by_name("password")
+	password.clear()
+	password.send_keys(config.password)
+
 	driver.find_element_by_xpath("/html/body/ui-layout/div/div/div[1]/et-login/et-login-sts/div/div/div/form/button").click()
 
 site_login()
