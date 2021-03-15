022670ee287b6e54182af21d3ddc686a96c8645e
petardmnv <petar.dmnv@gmail.com>
2021-03-16 00:19:08 +0200

    Add some research

---
+++ strategy.py
@@ -0,0 +1,23 @@
+'''
+Plan:
+	-Set strategy
+	-Backtest that strategy using old datqa from API
+	-Analizing the backtested results
+	-If the analise is ok(50%+) than usse the strategy
+
+Current API we are using - yahoo api
+Brokers:
+	-Etoro (no API)
+	-Webull (github API)
+	-Robinhood
+Basic strategies:
+  -5MA crossing 20MA
+  -Volume
+  -Trends
+  -News
+
+  Buying and selling stock:
+  	-Selling stock at 1-2% win and (???) loss
+  	-Stock should be (10-15)% of user portfolio
+
+'''
