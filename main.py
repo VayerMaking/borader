730029e80ea7254f642483f972db21b0d2faf973
petardmnv <petar.dmnv@gmail.com>
2021-03-22 23:52:55 +0200

    Add first steps in strategy.py and set up database

--- main.py
+++ main.py
@@ -1,11 +1,98 @@
-import yfinance as yf
 
-msft = yf.Ticker("DOGE-USD")
+import yfinance as yf
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, DateTime, Boolean, Numeric, MetaData
+
+engine = create_engine('mysql+pymysql://root:Asdf123er!@localhost:3306/borader')
+
+
+# Session = sessionmaker(bind=engine)
+# session = Session()
+#
+# Base = declarative_base()
+
+
+# class Product(Base):
+#     __tablename__ = 'products'
+#     id=Column(Integer, primary_key=True)
+#     title=Column('title', String(32))
+#     in_stock=Column('in_stock', Boolean)
+#     quantity=Column('quantity', Integer)
+#     price=Column('price', Numeric)
+#
+#     def __init__(self, title, in_stock, quantity, price):
+#         self.title = title
+#         self.in_stock = in_stock
+#         self.quantity = quantity
+#         self.price = price
+#
+# Product.__table__.create(bind=engine, checkfirst=True)
+
+
+# class Stock_Data(Base):
+#     __tablename__ = 'stock_data'
+#     id=Column(Integer, primary_key=True)
+#     datetime=Column('date', DateTime)
+#     open=Column('open', Numeric)
+#     high=Column('high', Numeric)
+#     low=Column('low', Numeric)
+#     close=Column('close', Numeric)
+#     volume=Column('volume', Integer)
+#
+#     def __init__(self, datetime, open, high, close, low, volume):
+#         self.datetime = datetime
+#         self.high = high
+#         self.open = open
+#         self.low = low
+#         self.close = close
+#         self.volume = volume
+#
+# Stock_Data.__table__.create(bind=engine, checkfirst=True)
+
+doge = yf.Ticker("DOGE-USD")
 tesla = yf.Ticker("TSLA")
 
-a =  msft.history(period="1d", interval="1m")
-b = tesla.history(period="1d", interval= "1m")
+dh =  doge.history(period="1d", interval="1m")
+# b = tesla.history(period="1d", interval= "1m")
+#
+#
+# a.to_csv('file1.csv')
+# b.to_csv('file2.csv')
+#print(tesla.info)
+th = tesla.history(period="1d", interval="1m")
 
+# print(tesla_historical.to_string())
+# print(tesla_historical["High"])
+# print(th.index)
+# for i in th.index:
+#
+#     #print(th.keys())
+#      #print(tesla_historical['Open'][i], tesla_historical['High'][i])
+#     #i  = str(i)
+#     #i = datetime.strptime(i, '%y-%m-%d %H:%M:%S-%f')
+#     print(i)
+#     session.add(Stock_Data(i, th['Open'][i], th['High'][i], th['Low'][i], th['Close'][i], th['Volume'][i]))
+#     session.commit()
 
-a.to_csv('file1.csv')
-b.to_csv('file2.csv')
+th.to_sql('asdf', con=engine, if_exists='replace')
+
+with engine.begin() as cn:
+   sql = """INSERT INTO myFinalTable (Datetime, Open, High, Low, Close, Volume, Dividends)
+            SELECT t.Datetime, t.Open, t.High, t.Low, t.Close, t.Volume, t.Dividends
+            FROM asdf t
+            WHERE NOT EXISTS
+                (SELECT 1 FROM myFinalTable f
+                 WHERE t.Datetime = f.Datetime)"""
+
+   cn.execute(sql)
+
+# asdf = Product("The Bourne Identity", True, 100, 99.99)
+#
+# session.add(asdf)
+
+# 10 - commit and close session
+# session.commit()
+# session.close()
--- strategy.py
+++ strategy.py
@@ -21,3 +21,70 @@ Basic strategies:
   	-Stock should be (10-15)% of user portfolio
 
 '''
+import pandas
+import datetime
+import pytz
+import os
+import time
+
+minutes = 240
+
+def get_curr_date():
+	my_timestamp = datetime.datetime.now()
+
+	old_timezone = pytz.timezone("EET")
+	new_timezone = pytz.timezone("US/Eastern")
+
+	localized_timestamp = old_timezone.localize(my_timestamp)
+	new_timezone_timestamp = localized_timestamp.astimezone(new_timezone)
+
+	l = list(new_timezone_timestamp.strftime("%z"))
+	l.insert(3, ":")
+	return new_timezone_timestamp.strftime("%Y-%m-%d %H:%M") + ":00" + ''.join(l)
+
+#curr_time = get_curr_date()
+#print(curr_time)
+#time.sleep(20)
+
+os.system('python main.py')
+
+df = pandas.read_csv('file2.csv')
+
+with open("file2.csv", 'r') as fd:
+	list_data = []
+	for line in fd:
+		l = list(line.split(","))
+		list_data.append(l)
+
+
+# Get data from database
+
+
+price_moved = 0;
+
+day_high_price = float(list_data[1][4])
+day_low_price = float(list_data[1][4])
+
+for i in range(2, len(list_data), 1):
+	price_before_minute = float(list_data[i - 1][4])
+	curr_price = float(list_data[i][4])
+	if curr_price > day_high_price:
+		day_high_price = curr_price
+	if curr_price < day_high_price:
+		day_low_price = curr_price
+	difference = price_before_minute - curr_price
+	if difference <= 0:
+		print(f"Down: {difference}")
+	else:
+		print(f"Up: {difference}")
+
+	price_moved += difference
+
+print(price_moved)
+print(day_high_price)
+print(day_low_price)
+print(len(list_data))
+
+
+
+
