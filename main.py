dcc627fdb8b2749a56f6b914ad72e33c1b40cd4d
VayerMaking <martin.vayer@gmail.com>
2021-04-19 17:57:33 +0300

    added threading and postgres changes

--- main.py
+++ main.py
@@ -1,63 +1,88 @@
-import yfinance as yf
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
-from sqlalchemy import Column, String, Integer, DateTime, Boolean, Numeric, MetaData, Table
+import yfinance as yf# stock data api
+from datetime import datetime, timedelta
+import time
+from sqlalchemy import create_engine#
+from sqlalchemy.ext.declarative import declarative_base#                            SQl
+from sqlalchemy.orm import sessionmaker, Session#                                                 Alchemy
+from sqlalchemy import Column, String, Integer, DateTime, Boolean, Numeric, MetaData, Table#   libs
 from sqlalchemy.ext.automap import automap_base
+from multiprocessing import Process # for threading
+import strategy #strategy.py
+import config # config module with usernames and passes
 
 Base = automap_base()
 
-class myFinalTable(Base):
-     __tablename__ = 'myFinalTable'
 
-     datetime = Column('DateTime', DateTime, primary_key=True)
-     open = Column('Open', Numeric)
-     high = Column('High', Numeric)
-     Low = Column('Low', Numeric)
-     Close = Column('Close', Numeric)
-     volume = Column('Volume', Integer)
-     dividends = Column('Dividends', Integer)
-     stock_splits = Column('Stock Splits', Integer)
+class myfinaltable(Base):
+     __tablename__ = 'myfinaltable'
 
+     datetime = Column('datetime', DateTime, primary_key=True)
+     open = Column('open', Numeric)
+     high = Column('high', Numeric)
+     Low = Column('low', Numeric)
+     Close = Column('close', Numeric)
+     volume = Column('volume', Integer)
+     dividends = Column('dividends', Integer)
+     stock_splits = Column('stock_splits', Integer)
 
-engine = create_engine('mysql+pymysql://root:vayertues@localhost:3306/borader')
+engine_str = 'postgresql://' + config.db_usr + ':' + config.db_pass + '@localhost/borader'
+engine = create_engine(engine_str)
 
-class myFinalTable(Base):
-    __table__ = Table('myFinalTable', Base.metadata,
+class myfinaltable(Base):
+    __table__ = Table('myfinaltable', Base.metadata,
                     autoload=True, autoload_with=engine)
 
-#Asdf = Base.classes.asdf
-#a = Base.classes.myFinalTable
-
-session = Session(engine)
+#session = Session(engine)
 Base.prepare(engine, reflect=True)
+#with Session(engine) as connection:
+#    res = connection.query(myfinaltable).first()
 
-res = session.query(myFinalTable).first()
-
+#Asdf = Base.classes.asdf
+#a = Base.classes.myfinaltable
 #print(res.Volume)
 
-doge = yf.Ticker("DOGE-USD")
-tesla = yf.Ticker("TSLA")
+def get_data_to_db():
+    while True:
+        doge = yf.Ticker("DOGE-USD")
+        tesla = yf.Ticker("TSLA")
 
-dh =  doge.history(period="1d", interval="1m")
+        dh =  doge.history(period="1d", interval="1m")
+        #past_time = timedelta(hours = 1)
+        today = datetime.now().date()
+        yesterday = today - timedelta(1)
+        print(today)
+        print(yesterday)
+        th = tesla.history(start=yesterday, end=today, interval="1m")
+        #th = tesla.history(period="1d", interval="1m")#, start=datetime.now().date())
+        #th = tesla.history(start= datetime.now() - past_time, end=datetime.now(), interval="1m")
+        th.reset_index(inplace=True)
+        print(th.columns)
+        th.rename(columns = {'Datetime' : 'datetime', 'Open' : 'open', 'High' : 'high', 'Low' : 'low', 'Close' : 'close', 'Volume' : 'volume', 'Dividends' : 'dividends'}, inplace = True)
+        print(th)
+        th.to_sql('asdf', con=engine, if_exists='append')
+
+        with engine.begin() as cn:
+            sql = """INSERT INTO myfinaltable (datetime, open, high, low, close, volume, dividends)
+                    SELECT t.Datetime, t.Open, t.High, t.Low, t.Close, t.Volume, t.Dividends
+                    FROM asdf t
+                    WHERE NOT EXISTS
+                        (SELECT 1 FROM myfinaltable f
+                        WHERE t.Datetime = f.Datetime)"""
+
+            cn.execute(sql)
+
+        print("completed a full cycle")
+        time.sleep(10)
+def analyze_data():
+    while True:
+        strategy.analyze()
 
 
-th = tesla.history(period="1d", interval="1m", start=datetime.now().date())
-
-
-th.to_sql('asdf', con=engine, if_exists='append')
-
-with engine.begin() as cn:
-   sql = """INSERT INTO myFinalTable (Datetime, Open, High, Low, Close, Volume, Dividends)
-            SELECT t.Datetime, t.Open, t.High, t.Low, t.Close, t.Volume, t.Dividends
-            FROM asdf t
-            WHERE NOT EXISTS
-                (SELECT 1 FROM myFinalTable f
-                 WHERE t.Datetime = f.Datetime)"""
-
-   cn.execute(sql)
-
+if __name__=='__main__':
+    p1 = Process(target = get_data_to_db)
+    p1.start()
+    p2 = Process(target = analyze_data)
+    p2.start()
 # asdf = Product("The Bourne Identity", True, 100, 99.99)
 #
 # session.add(asdf)
@@ -65,4 +90,3 @@ with engine.begin() as cn:
 # 10 - commit and close session
 # session.commit()
 # session.close()
-
